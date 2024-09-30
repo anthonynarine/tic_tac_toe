619b7816a2233f8be40860a1c68abdb6de46c155
anthonynarine <anarine83@gmail.com>
2024-09-30 10:50:56 -0400

    Remove tracked .pyc files and __pycache__ directories

--- backend/users/models.py
+++ backend/users/models.py
@@ -71,7 +71,7 @@ class CustomUser(AbstractUser):
             pass # If creating a new user, no action is neded
 
         # Resize avatar if necessary
-        if self.avatar:
+        if self.avatar and not self.avatar.name.endswith('default.jpg'):
             self.avatar = resize_image(self.avatar)
         
         super().save(*args, **kwargs)
